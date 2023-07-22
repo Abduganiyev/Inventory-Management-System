e359377cfe430493e031197a4387676cccb11a17
Abduganiyev <mr.show050@gmail.com>
2023-07-22 19:31:10 +0500

    added hints

--- src/main/java/com/example/inventorymanagementsys/controller/ItemController.java
+++ src/main/java/com/example/inventorymanagementsys/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.example.inventorymanagementsys.controller;
 import com.example.inventorymanagementsys.model.DTO.ItemDto;
 import com.example.inventorymanagementsys.model.Item;
 import com.example.inventorymanagementsys.service.CRUDService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,22 +17,25 @@ public class ItemController {
         this.service = service;
     }
 
-
+    @Operation(summary = "This API for getting list items")
     @GetMapping()
     public List<Item> getItems(){
         return service.findAll();
     }
 
+    @Operation(summary = "This API for adding new item")
     @PostMapping
     public Item addItem(@RequestBody ItemDto dto) {
         return service.save(dto);
     }
 
+    @Operation(summary = "This API for sorting list and get list items")
     @PostMapping("sortByName")
     public List<Item> SortByName() {
         return service.SortByName();
     }
 
+    @Operation(summary = "This API for delete item with id")
     @DeleteMapping("/{item_id}")
     public String deleteItem(@PathVariable Long item_id){
         service.deleteItem(item_id);
